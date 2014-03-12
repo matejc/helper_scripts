bdbe572da71e95c612451a9996aa880e1484c383
Matej Cotman <cotman.matej@gmail.com>
2014-03-12 13:16:33 +0100

    Update searx.py

--- bin/searx.py
+++ bin/searx.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
 import json
+import os
 import re
 import subprocess
 import urllib
 import urllib2
 
-WWW_SEARX = 'http://searx.scriptores.com'
-BROWSER = 'vimprobable2'
+WWW_SEARX = u'http://searx.scriptores.com'
+BROWSER = u'chromium'
+
+devnull = open(os.devnull)
+
+
+def clipboard(selection=False):
+    cb_cmd = [subprocess.check_output(["which", 'xclip']).strip()]
+    cb_cmd += ['-out', '-selection']
+    if selection:
+        cb_cmd += ['primary']
+    else:
+        cb_cmd += ['clipboard']
+    p = subprocess.Popen(
+        cb_cmd,
+        stdin=devnull, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, _ = p.communicate()
+    return stdout.decode('utf-8').strip('\n')
 
 
 def browser(args=[]):
@@ -55,14 +73,15 @@ def searx(query):
     return results
 
 
-query = dmenu(['-p', 'searx:'])
-results = searx(query)
-options = []
-for index, title, url in results:
-    options += [u'{} - {} [{}]'.format(index, title, url)]
+query = dmenu(['-p', 'searx:', '-l', '3'], [clipboard(selection=True), clipboard()])
+if query:
+    results = searx(query)
+    options = [u'0 - Searx - [{}/?{}]'.format(WWW_SEARX, urllib.urlencode({u'q': query}))]
+    for index, title, url in results:
+        options += [u'{} - {} - [{}]'.format(index + 1, title, url)]
 
-target = dmenu(['-l', '10'], options)
+    target = dmenu(['-l', '10'], options)
 
-browser(
-    [results[int(re.match('(\d+)\ \- ', target).groups()[0])][2], ]
-)
+    browser(
+        [re.match('^\d+\ \- .+\ \-\ \[(.+)\]$', target).groups()[0], ]
+    )
