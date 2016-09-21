56339db9bceaaf524ec08432863c56cc49e9af17
Matej Cotman <cotman.matej@gmail.com>
2016-09-22 01:10:53 +0200

    nix: cleanup and add new key bindings

--- nixes/vim.nix
+++ nixes/vim.nix
@@ -14,6 +14,7 @@ vimrcConfig = {
         "The_NERD_tree"
         "taglist"
         "youcompleteme"
+        "molokai"
     ];
   }];
   customRC = ''
@@ -30,7 +31,7 @@ vimrcConfig = {
     let g:airline_theme='dark'
     let g:airline_detect_modified=1
     let g:airline_powerline_fonts = 1
-    set guifont=Source\ Code\ Pro\ for\ Powerline\ Regular\ 9
+    set guifont=Source\ Code\ Pro\ for\ Powerline\ Regular\ 11
 
     set laststatus=2
     set number
@@ -83,6 +84,8 @@ vimrcConfig = {
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     " Enable syntax highlighting
     syntax enable
+    let g:molokai_original = 1
+    colorscheme molokai
 
     " Set utf8 as standard encoding and en_US as the standard language
     set encoding=utf8
@@ -116,111 +119,6 @@ vimrcConfig = {
 
     set cursorline
 
-
-
-
-
-    set background=dark
-    highlight clear
-    set t_Co=256
-    let g:colors_name = "monokai"
-    hi Cursor ctermfg=235 ctermbg=231 cterm=NONE guifg=#272822 guibg=#f8f8f0 gui=NONE
-    hi Visual ctermfg=NONE ctermbg=59 cterm=NONE guifg=NONE guibg=#49483e gui=NONE
-    hi CursorLine ctermfg=NONE ctermbg=237 cterm=NONE guifg=NONE guibg=#3c3d37 gui=NONE
-    hi CursorColumn ctermfg=NONE ctermbg=237 cterm=NONE guifg=NONE guibg=#3c3d37 gui=NONE
-    hi ColorColumn ctermfg=NONE ctermbg=237 cterm=NONE guifg=NONE guibg=#3c3d37 gui=NONE
-    hi LineNr ctermfg=102 ctermbg=237 cterm=NONE guifg=#90908a guibg=#3c3d37 gui=NONE
-    hi VertSplit ctermfg=241 ctermbg=241 cterm=NONE guifg=#64645e guibg=#64645e gui=NONE
-    hi MatchParen ctermfg=197 ctermbg=NONE cterm=underline guifg=#f92672 guibg=NONE gui=underline
-    hi StatusLine ctermfg=231 ctermbg=241 cterm=bold guifg=#f8f8f2 guibg=#64645e gui=bold
-    hi StatusLineNC ctermfg=231 ctermbg=241 cterm=NONE guifg=#f8f8f2 guibg=#64645e gui=NONE
-    hi Pmenu ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi PmenuSel ctermfg=NONE ctermbg=59 cterm=NONE guifg=NONE guibg=#49483e gui=NONE
-    hi IncSearch ctermfg=235 ctermbg=186 cterm=NONE guifg=#272822 guibg=#e6db74 gui=NONE
-    hi Search ctermfg=NONE ctermbg=NONE cterm=underline guifg=NONE guibg=NONE gui=underline
-    hi Directory ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi Folded ctermfg=242 ctermbg=235 cterm=NONE guifg=#75715e guibg=#272822 gui=NONE
-    hi SignColumn ctermfg=NONE ctermbg=237 cterm=NONE guifg=NONE guibg=#3c3d37 gui=NONE
-    hi Normal ctermfg=231 ctermbg=235 cterm=NONE guifg=#f8f8f2 guibg=#272822 gui=NONE
-    hi Boolean ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi Character ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi Comment ctermfg=242 ctermbg=NONE cterm=NONE guifg=#75715e guibg=NONE gui=NONE
-    hi Conditional ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi Constant ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi Define ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi DiffAdd ctermfg=231 ctermbg=64 cterm=bold guifg=#f8f8f2 guibg=#46830c gui=bold
-    hi DiffDelete ctermfg=88 ctermbg=NONE cterm=NONE guifg=#8b0807 guibg=NONE gui=NONE
-    hi DiffChange ctermfg=NONE ctermbg=NONE cterm=NONE guifg=#f8f8f2 guibg=#243955 gui=NONE
-    hi DiffText ctermfg=231 ctermbg=24 cterm=bold guifg=#f8f8f2 guibg=#204a87 gui=bold
-    hi ErrorMsg ctermfg=231 ctermbg=197 cterm=NONE guifg=#f8f8f0 guibg=#f92672 gui=NONE
-    hi WarningMsg ctermfg=231 ctermbg=197 cterm=NONE guifg=#f8f8f0 guibg=#f92672 gui=NONE
-    hi Float ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi Function ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi Identifier ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=italic
-    hi Keyword ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi Label ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi NonText ctermfg=59 ctermbg=236 cterm=NONE guifg=#49483e guibg=#31322c gui=NONE
-    hi Number ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi Operator ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi PreProc ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi Special ctermfg=231 ctermbg=NONE cterm=NONE guifg=#f8f8f2 guibg=NONE gui=NONE
-    hi SpecialComment ctermfg=242 ctermbg=NONE cterm=NONE guifg=#75715e guibg=NONE gui=NONE
-    hi SpecialKey ctermfg=59 ctermbg=237 cterm=NONE guifg=#49483e guibg=#3c3d37 gui=NONE
-    hi Statement ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi StorageClass ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=italic
-    hi String ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi Tag ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi Title ctermfg=231 ctermbg=NONE cterm=bold guifg=#f8f8f2 guibg=NONE gui=bold
-    hi Todo ctermfg=95 ctermbg=NONE cterm=inverse,bold guifg=#75715e guibg=NONE gui=inverse,bold
-    hi Type ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi Underlined ctermfg=NONE ctermbg=NONE cterm=underline guifg=NONE guibg=NONE gui=underline
-    hi rubyClass ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi rubyFunction ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi rubyInterpolationDelimiter ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi rubySymbol ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi rubyConstant ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=italic
-    hi rubyStringDelimiter ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi rubyBlockParameter ctermfg=208 ctermbg=NONE cterm=NONE guifg=#fd971f guibg=NONE gui=italic
-    hi rubyInstanceVariable ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi rubyInclude ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi rubyGlobalVariable ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi rubyRegexp ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi rubyRegexpDelimiter ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi rubyEscape ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi rubyControl ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi rubyClassVariable ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi rubyOperator ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi rubyException ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi rubyPseudoVariable ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi rubyRailsUserClass ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=italic
-    hi rubyRailsARAssociationMethod ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi rubyRailsARMethod ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi rubyRailsRenderMethod ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi rubyRailsMethod ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi erubyDelimiter ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi erubyComment ctermfg=95 ctermbg=NONE cterm=NONE guifg=#75715e guibg=NONE gui=NONE
-    hi erubyRailsMethod ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi htmlTag ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi htmlEndTag ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi htmlTagName ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi htmlArg ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi htmlSpecialChar ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi javaScriptFunction ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=italic
-    hi javaScriptRailsFunction ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi javaScriptBraces ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi yamlKey ctermfg=197 ctermbg=NONE cterm=NONE guifg=#f92672 guibg=NONE gui=NONE
-    hi yamlAnchor ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi yamlAlias ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-    hi yamlDocumentHeader ctermfg=186 ctermbg=NONE cterm=NONE guifg=#e6db74 guibg=NONE gui=NONE
-    hi cssURL ctermfg=208 ctermbg=NONE cterm=NONE guifg=#fd971f guibg=NONE gui=italic
-    hi cssFunctionName ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi cssColor ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi cssPseudoClassId ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi cssClassName ctermfg=148 ctermbg=NONE cterm=NONE guifg=#a6e22e guibg=NONE gui=NONE
-    hi cssValueLength ctermfg=141 ctermbg=NONE cterm=NONE guifg=#ae81ff guibg=NONE gui=NONE
-    hi cssCommonAttr ctermfg=81 ctermbg=NONE cterm=NONE guifg=#66d9ef guibg=NONE gui=NONE
-    hi cssBraces ctermfg=NONE ctermbg=NONE cterm=NONE guifg=NONE guibg=NONE gui=NONE
-
     function! s:get_visual_selection()
       " Why is this not a built-in Vim script function?!
       let [lnum1, col1] = getpos("'<")[1:2]
@@ -230,8 +128,6 @@ vimrcConfig = {
       let lines[0] = lines[0][col1 - 1:]
       return join(lines, "\n")
     endfunction
-
-    highlight Pmenu ctermfg=0 ctermbg=3 guifg=#ffffff guibg=#0000ff
 
     map <PageUp> 30<up>
     map <PageDown> 30<down>
@@ -247,11 +143,16 @@ vimrcConfig = {
     map <up> k
     map <down> j
 
-    vmap <C-c> "+yi
+    vmap <C-c> "+y
+    nmap <C-c> "+yy
+    imap <C-c> <esc>"+yyli
+
     vmap <C-x> "+c
     nmap <C-x> "+dd
     imap <C-x> <esc>"+ddi
+
     vmap <C-v> c<ESC>"+p
+    nmap <C-v> "+p
     imap <C-v> <C-r><C-o>+
 
     map <C-z> u
@@ -265,8 +166,10 @@ vimrcConfig = {
     map! <C-s> <esc>:w<Return>
     "map <C-d> /\<<C-R>=expand('<cword>')<CR>\><CR>
     map <C-d> Yp
-    map <C-f> /
+    map <leader>f :grep -R '<C-R>=expand('<cword>')<CR>' .
+    map <C-f> /<C-R>=expand('<cword>')<CR>
     map <C-a> <esc>ggVG<CR>
+    map <C-h> :%s/<C-R>=expand('<cword>')<CR>/<C-R>=expand('<cword>')<CR>/g
 
     nnoremap <C-down> :m .+1<CR>==
     nnoremap <C-up> :m .-2<CR>==
@@ -314,53 +217,56 @@ vimrcConfig = {
     " File type detection. Indent based on filetype. Recommended.
     filetype plugin indent on
 
-    map <C-\> :NERDTreeToggle<CR> 
+    map <C-\> :NERDTreeToggle<CR>
 
     nmap <BS> i<BS>
-    map <C-t> :TlistToggle<CR> 
+    map <C-t> :TlistToggle<CR>
 
     let g:NERDCustomDelimiters = { 'nix': { 'left': '#' } }
 
     vmap <C-Insert> <leader>cc
     vmap <C-S-Insert> <leader>cu
 
-    function! FindProjectName()
-      let s:name = getcwd()
-      if !isdirectory(".git")
-        let s:name = substitute(finddir(".git", ".;"), "/.git", "", "")
-      end
-      if s:name != ""
-        let s:name = matchstr(s:name, ".*", strridx(s:name, "/") + 1)
-      end
-      return s:name
-    endfunction
-
-    " Sessions only restored if we start Vim without args.
-    function! RestoreSession(name)
-      if a:name != ""
-        if filereadable($HOME . "/.vim/sessions/" . a:name)
-          execute 'source ' . $HOME . "/.vim/sessions/" . a:name
-        end
-      end
-    endfunction
-
-    " Sessions only saved if we start Vim without args.
-    function! SaveSession(name)
-      if a:name != ""
-        execute 'mksession! ' . $HOME . '/.vim/sessions/' . a:name
-      end
-    endfunction
-
-    " Restore and save sessions.
-    if argc() == 0
-      autocmd VimEnter * call RestoreSession(FindProjectName())
-      autocmd VimLeave * call SaveSession(FindProjectName())
-    end
-
     set splitright
     nnoremap <leader><right> <C-W><C-L>
     nnoremap <leader><left> <C-W><C-H>
     nnoremap <leader>c :vsp<CR>
+
+    let g:syntastic_javascript_jshint_exec = '/home/matejc/.npm-packages/bin/jshint'
+    let g:syntastic_jshint_exec = '/home/matejc/.npm-packages/bin/jshint'
+
+
+    function! MakeSession()
+      let b:sessiondir = $HOME . "/.vim/sessions" . getcwd()
+      if (filewritable(b:sessiondir) != 2)
+        exe 'silent !mkdir -p ' b:sessiondir
+        redraw!
+      endif
+      let b:filename = b:sessiondir . '/session.vim'
+      exe "mksession! " . b:filename
+    endfunction
+
+    function! LoadSession()
+      let b:sessiondir = $HOME . "/.vim/sessions" . getcwd()
+      let b:sessionfile = b:sessiondir . "/session.vim"
+      if (filereadable(b:sessionfile))
+        exe 'source ' b:sessionfile
+      else
+        echo "No session loaded."
+      endif
+    endfunction
+
+    " Adding automatons for when entering or leaving Vim
+    au VimEnter * nested :call LoadSession()
+    au VimLeave * :call MakeSession()
+
+    au FileType javascript setl sw=2 sts=2 et
+
+    " indent: mixed indent within a line
+    " long:   overlong lines
+    " trailing: trailing whitespace
+    " mixed-indent-file: different indentation in different lines
+    let g:airline#extensions#whitespace#checks = [ 'trailing', 'mixed-indent-file' ]
 
     exec 'source '.fnameescape($HOME.'/.vimrc')
     '';
