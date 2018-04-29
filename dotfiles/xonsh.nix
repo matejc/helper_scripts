af100c4de73bbb27b332c826bf1ae4c5cf25c400
Matej Cotman <cotman.matej@gmail.com>
2018-04-29 13:24:41 +0200

    feat(xonsh): ctrl+r completion

--- dotfiles/xonsh.nix
+++ dotfiles/xonsh.nix
@@ -11,6 +11,25 @@ let
 in {
   target = "${variables.homeDir}/.xonshrc";
   source = pkgs.writeScript "xonshrc" ''
+    import json
+    from os import listdir
+    from os.path import join
+    from operator import itemgetter
+    from prompt_toolkit.keys import Keys
+    from prompt_toolkit.filters import Condition
+    from prompt_toolkit.completion import Completion
+    from prompt_toolkit.document import Document
+
+    hist_dir = __xonsh_env__['XONSH_DATA_DIR']
+
+    # docs http://python-prompt-toolkit.readthedocs.io/en/master/pages/reference.html
+
+    $PATH += ["/home/matejc/bin", "/run/wrappers/bin", "/home/matejc/.nix-profile/bin", "/nix/var/nix/profiles/default/bin", "/run/current-system/sw/bin", "/etc/profiles/per-user/matejc/bin"]
+
+    history_search_state = {
+      'text': None,
+      'history': []
+    }
 
     def battery():
       batstatus = int($(batstatus))
@@ -81,6 +100,43 @@ in {
       $RIGHT_PROMPT = rprompt(seconds=seconds, telepresence_pod=telepresence_pod, rtn=rtn)
       $TITLE = title()
 
+    def del_duplicates(seq):
+      seen = set()
+      seen_add = seen.add
+      return [x for x in seq if not (x in seen or seen_add(x))]
+
+    def getHistory(text = ""):
+      files = [join(hist_dir,f) for f in listdir(hist_dir)
+               if f.startswith('xonsh-') and f.endswith('.json')]
+
+      fileHist = [json.load(open(f))['data']['cmds'] for f in files]
+
+      commands = [(c['inp'].replace('\n', ""), c['ts'][0])
+                  for commands in fileHist for c in commands if c]
+
+      commands.sort(key=itemgetter(1))
+
+      numOfCommands = len(commands)
+      digits = len(str(numOfCommands))
+
+      return [c[0] for c in commands if c[0].find(text) != -1]
+
+    @events.on_ptk_create
+    def custom_keybindings(bindings, **kw):
+      handler = bindings.registry.add_binding
+
+      @Condition
+      def has_text(cli):
+        return len(cli.current_buffer.document.text) != 0
+
+      @handler(Keys.ControlR, filter=has_text)
+      def history_search_activate(event):
+        history_search_state['text'] = event.cli.current_buffer.document.text
+        history_search_state['history'] = del_duplicates(getHistory(text=history_search_state['text']))
+        history_search_state['history'].reverse()
+        completions = [Completion(text=t, start_position=(len(history_search_state['text'])*-1)) for t in history_search_state['history']]
+        event.cli.current_buffer.set_completions(completions)
+
     $PROMPT = prompt()
     $RIGHT_PROMPT = rprompt()
     $TITLE = title()
@@ -89,5 +145,7 @@ in {
     $CASE_SENSITIVE_COMPLETIONS = False
     $VC_BRANCH_TIMEOUT = 0.5
     $PERL5LIB = "${pkgs.git}/share/perl5" + ":" + $PERL5LIB
+    $XONSH_SHOW_TRACEBACK = True
+    $COMPLETIONS_DISPLAY = "single"
   '';
 }
