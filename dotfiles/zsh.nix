76c41c4a1c952c56a34ceb66a37d68c799bb4530
Matej Cotman <cotman.matej@gmail.com>
2018-09-10 14:18:50 +0200

    feat(zsh): add zcompile to zlogin

--- dotfiles/zsh.nix
+++ dotfiles/zsh.nix
@@ -116,4 +116,28 @@
 
     setopt histignorespace
   '';
+} {
+  target = "${variables.homeDir}/.zlogin";
+  source = pkgs.writeText "zlogin" ''
+  (
+    # Function to determine the need of a zcompile. If the .zwc file
+    # does not exist, or the base file is newer, we need to compile.
+    # These jobs are asynchronous, and will not impact the interactive shell
+    zcompare() {
+      if [[ -s ''${1} && ( ! -s ''${1}.zwc || ''${1} -nt ''${1}.zwc) ]]; then
+        zcompile ''${1}
+      fi
+    }
+
+    setopt EXTENDED_GLOB
+
+    # zcompile the completion cache; siginificant speedup.
+    for file in ${variables.homeDir}/.zcomp^(*.zwc)(.); do
+      zcompare ''${file}
+    done
+
+    # zcompile .zshrc
+    zcompare ${variables.homeDir}/.zshrc
+  ) &!
+  '';
 }]
