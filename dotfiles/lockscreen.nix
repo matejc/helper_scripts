b64d5198e8b4ec98f0dd721d827e66e7aee613e8
Matej Cotman <cotman.matej@gmail.com>
2019-10-20 20:23:31 +0300

    feat(lockscreen): do not lock if active window is fullscreen

--- dotfiles/lockscreen.nix
+++ dotfiles/lockscreen.nix
@@ -6,13 +6,13 @@
 
     ${pkgs.procps}/bin/pgrep i3lock
 
-    if [ $? -ne 0 ]
+    if [ $? -ne 0 ] && ! ${variables.homeDir}/bin/is-win-fullscreen
     then
       revert() {
         ${pkgs.xorg.xset}/bin/xset dpms 0 0 0
       }
       trap revert HUP INT TERM
-      ${pkgs.xorg.xset}/bin/xset +dpms dpms 5 5 5
+      ${pkgs.xorg.xset}/bin/xset +dpms dpms 2 2 2
       sleep 0.25
       /run/wrappers/bin/i3lock -i ${variables.lockImage} --nofork
       revert
@@ -26,5 +26,13 @@
     export PATH="${lib.makeBinPath [ pkgs.gnugrep pkgs.gawk pkgs.findutils pkgs.systemd ]}"
 
     loginctl list-sessions | grep '^\ ' | awk '{print $1}' | xargs -i loginctl lock-session '{}'
+  '';
+} {
+  target = "${variables.homeDir}/bin/is-win-fullscreen";
+  source = pkgs.writeScript "is-win-fullscreen.sh" ''
+    #!${pkgs.stdenv.shell}
+
+    activ_win_id="$(${pkgs.xorg.xprop}/bin/xprop -root _NET_ACTIVE_WINDOW)"
+    ${pkgs.xorg.xprop}/bin/xprop -id ''${activ_win_id:40:9} | ${pkgs.gnugrep}/bin/grep -q _NET_WM_STATE_FULLSCREEN
   '';
 }]
