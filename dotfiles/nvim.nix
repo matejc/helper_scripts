7c2f1b8235f9efa67e8ce0b70f9d6f4e61b5ccb8
Matej Cotman <matej.cotman@eficode.com>
2020-07-03 15:17:05 +0300

    update nvim

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -434,6 +434,22 @@ EOF
     " let g:netrw_browse_split = 4
     " let g:netrw_altv = 1
     " let g:netrw_winsize = 25
+    let g:netrw_keepdir = 0
+
+    " CtrlP auto cache clearing.
+    " ----------------------------------------------------------------------------
+    function! SetupCtrlP()
+      if exists("g:loaded_ctrlp") && g:loaded_ctrlp
+        augroup CtrlPExtension
+          autocmd!
+          autocmd FocusGained  * CtrlPClearCache
+          autocmd BufWritePost * CtrlPClearCache
+        augroup END
+      endif
+    endfunction
+    if has("autocmd")
+      autocmd VimEnter * :call SetupCtrlP()
+    endif
  '';
 
   neovim-unwrapped = pkgs.neovim-unwrapped.overrideDerivation (old: {
