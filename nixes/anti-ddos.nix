e9b83ddb0ae2c4acbf8e67973d43b022dedec164
Matej Cotman <matej@matejc.com>
2021-01-28 00:38:14 +0200

    update

--- nixes/anti-ddos.nix
+++ nixes/anti-ddos.nix
@@ -3,65 +3,69 @@
   # Source: https://javapipe.com/blog/iptables-ddos-protection/
 
   networking.firewall.extraCommands = ''
+    iptables_add() {
+      iptables -C $@ || iptables -A $@
+    }
+
     ### 1: Drop invalid packets ###
-    iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+    iptables_add PREROUTING -t mangle -m conntrack --ctstate INVALID -j DROP
 
     ### 2: Drop TCP packets that are new and are not SYN ###
-    iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+    iptables_add PREROUTING -t mangle -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 
     ### 3: Drop SYN packets with suspicious MSS value ###
-    iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+    iptables_add PREROUTING -t mangle -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 
     ### 4: Block packets with bogus TCP flags ###
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-    iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags FIN,ACK FIN -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags ACK,URG URG -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags ACK,PSH PSH -j DROP
+    iptables_add PREROUTING -t mangle -p tcp --tcp-flags ALL NONE -j DROP
 
     ### 5: Block spoofed packets ###
-    iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
-    iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
-    iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
-    iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
-    iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
-    iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
-    iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
-    iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
-    iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+    iptables_add PREROUTING -t mangle -s 224.0.0.0/3 -j DROP
+    iptables_add PREROUTING -t mangle -s 169.254.0.0/16 -j DROP
+    iptables_add PREROUTING -t mangle -s 172.16.0.0/12 -j DROP
+    iptables_add PREROUTING -t mangle -s 192.0.2.0/24 -j DROP
+    iptables_add PREROUTING -t mangle -s 192.168.0.0/16 -j DROP
+    iptables_add PREROUTING -t mangle -s 10.0.0.0/8 -j DROP
+    iptables_add PREROUTING -t mangle -s 0.0.0.0/8 -j DROP
+    iptables_add PREROUTING -t mangle -s 240.0.0.0/5 -j DROP
+    iptables_add PREROUTING -t mangle -s 127.0.0.0/8 ! -i lo -j DROP
 
     ### 6: Drop ICMP (you usually don't need this protocol) ###
-    iptables -t mangle -A PREROUTING -p icmp -j DROP
+    iptables_add PREROUTING -t mangle -p icmp -j DROP
 
     ### 7: Drop fragments in all chains ###
-    iptables -t mangle -A PREROUTING -f -j DROP
+    iptables_add PREROUTING -t mangle -f -j DROP
 
     ### 8: Limit connections per source IP ###
-    iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
+    iptables_add INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
 
     ### 9: Limit RST packets ###
-    iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-    iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+    iptables_add INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+    iptables_add INPUT -p tcp --tcp-flags RST RST -j DROP
 
     ### 10: Limit new TCP connections per second per source IP ###
-    iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
-    iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+    iptables_add INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
+    iptables_add INPUT -p tcp -m conntrack --ctstate NEW -j DROP
 
     ### 11: Use SYNPROXY on all ports (disables connection limiting rule) ###
-    iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
-    iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
-    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+    #iptables_add PREROUTING -t raw -p tcp -m tcp --syn -j CT --notrack
+    #iptables_add INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
+    #iptables_add INPUT -m conntrack --ctstate INVALID -j DROP
 
     ### SSH brute-force protection ###
-    iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
-    iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+    iptables_add INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+    iptables_add INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
 
     ### Protection against port scanning ###
     iptables -n --list port-scanning || iptables -N port-scanning
-    iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
-    iptables -A port-scanning -j DROP
+    iptables_add port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+    iptables_add port-scanning -j DROP
   '';
 
   boot.kernel.sysctl = {
