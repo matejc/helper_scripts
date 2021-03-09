c6cfb89050a980eeed77b4cb2998c5ebc4774f9d
Matej Cotman <matej@matejc.com>
2021-03-09 13:04:11 +0200

    cmdheight

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -59,8 +59,7 @@ let
     set nobackup
     set nowritebackup
 
-    " Better display for messages
-    " set cmdheight=1
+    set cmdheight=2
 
     " You will have bad experience for diagnostic messages when it's default 4000.
     set updatetime=300
