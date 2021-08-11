ad17f9014a0af2fa0a8c9aea2d9cebe300b06993
Matej Cotman <matej.cotman@eficode.com>
2021-08-11 16:05:28 +0300

    feat(nvim,lsp): replace pylsp with pyright

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -565,7 +565,7 @@ end
 
 nvim_lsp["kotlin_language_server"].setup { on_attach = on_attach; cmd = {"${kotlin-language-server}/bin/kotlin-language-server"} }
 nvim_lsp["rnix"].setup { on_attach = on_attach; cmd = {"${pkgs.rnix-lsp}/bin/rnix-lsp"} }
-nvim_lsp["pylsp"].setup { on_attach = on_attach; cmd = {"${pkgs.python3Packages.python-lsp-server}/bin/pylsp"} }
+nvim_lsp["pyright"].setup { on_attach = on_attach; cmd = {"${pkgs.pyright}/bin/pyright-langserver", "--stdio"} }
 nvim_lsp["bashls"].setup { on_attach = on_attach; cmd = {"${variables.homeDir}/.npm-packages/bin/bash-language-server", "start"} }
 nvim_lsp["dockerls"].setup { on_attach = on_attach; cmd = {"${variables.homeDir}/.npm-packages/bin/docker-langserver", "--stdio"} }
 nvim_lsp["yamlls"].setup { on_attach = on_attach; cmd = {"${variables.homeDir}/.npm-packages/bin/yaml-language-server", "--stdio"} }
