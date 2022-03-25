3d97bf399174100aa330ff7ca97e2dfef4419c8c
Matej Cotman <cotman.matej@gmail.com>
2022-03-25 20:04:41 +0200

    feat: update

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -1085,8 +1085,8 @@ require("neo-tree").setup({
   filesystem = {
     filtered_items = {
       visible = false, -- when true, they will just be displayed differently than normal items
-      hide_dotfiles = true,
-      hide_gitignored = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
       hide_by_name = {
         ".DS_Store",
         "thumbs.db"
