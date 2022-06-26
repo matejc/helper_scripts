8aad16e9685a8ac71f4d3a26b372cc0e6ba83a6f
Matej Cotman <matej@matejc.com>
2022-06-26 04:33:40 +0300

    feat(ark): add game

--- nixes/ark.nix
+++ nixes/ark.nix
@@ -1,50 +1,65 @@
 {config, pkgs, lib, ...}:
 let
   cfg = config.services.ark;
-in {
-  users.users.ark = {
-    home = "/var/lib/ark";
-    createHome = true;
-    isSystemUser = true;
-    group = "ark";
-  };
-  users.groups.ark = {};
-
-  nixpkgs.config.allowUnfree = true;
-
-  networking = {
-    firewall = {
-      allowedTCPPorts = [ 27020 ];
-      allowedUDPPorts = [ 27015 ];
-      allowedUDPPortRanges = [ { from = 7777; to = 7782; } ];
+in
+{
+  options = {
+    enable = lib.mkEnableOption "Enable ARK Dedicated Server";
+    password = lib.mkOption {
+      type = lib.types.str;
+      description = "Server password";
+    };
+    adminPassword = lib.mkOption {
+      type = lib.types.str;
+      description = "Server Admin password";
     };
   };
 
-
-  systemd.services.ark = {
-    wantedBy = [ "multi-user.target" ];
-    preStart = ''
-      ${pkgs.steamcmd}/bin/steamcmd \
-        +login anonymous \
-        +force_install_dir /var/lib/ark/ARKDedicatedServer \
-        +app_update 376030 validate \
-        +quit
-      ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 /var/lib/ark/ARKDedicatedServer/ShooterGame/Binaries/Linux/ShooterGameServer
-    '';
-    script = ''
-      /var/lib/ark/ARKDedicatedServer/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=matejc?ServerPassword=${cfg.password}?ServerAdminPassword=${cfg.adminPassword}?MaxPlayers=10 -server -log
-    '';
-    serviceConfig = {
-      Nice = "-5";
-      Restart = "always";
-      User = "ark";
-      WorkingDirectory = "/var/lib/ark";
-      LimitNOFILE = 100000;
-      ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
-      ExecStop= "${pkgs.coreutils}/bin/kill -s INT $MAINPID";
+  config = lib.mkIf cfg.enable {
+    users.users.ark = {
+      home = "/var/lib/ark";
+      createHome = true;
+      isSystemUser = true;
+      group = "ark";
     };
-    environment = {
-      LD_LIBRARY_PATH="ARKDedicatedServer/ShooterGame/linux64:ARKDedicatedServer/ShooterGame/Engine/Binaries/Linux:ARKDedicatedServer/ShooterGame/Engine/Binaries/ThirdParty/PhysX3/Linux/x86_64-unknown-linux-gnu/";
+    users.groups.ark = {};
+
+    nixpkgs.config.allowUnfree = true;
+
+    networking = {
+      firewall = {
+        allowedTCPPorts = [ 27020 ];
+        allowedUDPPorts = [ 27015 ];
+        allowedUDPPortRanges = [ { from = 7777; to = 7782; } ];
+      };
+    };
+
+
+    systemd.services.ark = {
+      wantedBy = [ "multi-user.target" ];
+      preStart = ''
+        ${pkgs.steamcmd}/bin/steamcmd \
+          +login anonymous \
+          +force_install_dir /var/lib/ark/ARKDedicatedServer \
+          +app_update 376030 validate \
+          +quit
+        ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 /var/lib/ark/ARKDedicatedServer/ShooterGame/Binaries/Linux/ShooterGameServer
+      '';
+      script = ''
+        /var/lib/ark/ARKDedicatedServer/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=matejc?ServerPassword=${cfg.password}?ServerAdminPassword=${cfg.adminPassword}?MaxPlayers=10 -server -log
+      '';
+      serviceConfig = {
+        Nice = "-5";
+        Restart = "always";
+        User = "ark";
+        WorkingDirectory = "/var/lib/ark";
+        LimitNOFILE = 100000;
+        ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
+        ExecStop= "${pkgs.coreutils}/bin/kill -s INT $MAINPID";
+      };
+      environment = {
+        LD_LIBRARY_PATH="ARKDedicatedServer/ShooterGame/linux64:ARKDedicatedServer/ShooterGame/Engine/Binaries/Linux:ARKDedicatedServer/ShooterGame/Engine/Binaries/ThirdParty/PhysX3/Linux/x86_64-unknown-linux-gnu/";
+      };
     };
   };
 }
