3d9c40c9839fe696d8068d8b2ae85ebfbbc82049
Matej Cotman <matej@matejc.com>
2023-02-01 17:52:07 +0200

    feat: update

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -640,9 +640,13 @@ EOF
     inoremap <C-k> <C-o>"_dd
     vnoremap <C-k> "_d
 
-    " nnoremap <C-x> dd
-    " inoremap <C-x> <esc>ddi
-    " vnoremap <C-x> d
+    augroup MyCTRLX
+      autocmd!
+      autocmd VimEnter * iunmap <C-X><C-A>
+    augroup END
+    inoremap <C-x> <C-o>dd
+    nnoremap <C-x> dd
+    vnoremap <C-x> d
 
     " nnoremap <C-a> gg0vG$
     " inoremap <C-a> <C-o>gg0vG$
@@ -663,6 +667,53 @@ EOF
     " nmap <C-S-Down> :copy .<cr>
     " vmap <C-S-Down> :copy '><cr>
     " imap <C-S-Down> <esc>:copy .<cr>i
+
+    function! MoveLineUp()
+      call MoveLineOrVisualUp(".", "")
+    endfunction
+
+    function! MoveLineDown()
+      call MoveLineOrVisualDown(".", "")
+    endfunction
+
+    function! MoveVisualUp()
+      call MoveLineOrVisualUp("'<", "'<,'>")
+      normal gv
+    endfunction
+
+    function! MoveVisualDown()
+      call MoveLineOrVisualDown("'>", "'<,'>")
+      normal gv
+    endfunction
+
+    function! MoveLineOrVisualUp(line_getter, range)
+      let l_num = line(a:line_getter)
+      if l_num - v:count1 - 1 < 0
+        let move_arg = "0"
+      else
+        let move_arg = a:line_getter." -".(v:count1 + 1)
+      endif
+      call MoveLineOrVisualUpOrDown(a:range."move ".move_arg)
+    endfunction
+
+    function! MoveLineOrVisualDown(line_getter, range)
+      let l_num = line(a:line_getter)
+      if l_num + v:count1 > line("$")
+        let move_arg = "$"
+      else
+        let move_arg = a:line_getter." +".v:count1
+      endif
+      call MoveLineOrVisualUpOrDown(a:range."move ".move_arg)
+    endfunction
+
+    function! MoveLineOrVisualUpOrDown(move_arg)
+      let col_num = virtcol(".")
+      execute "silent! ".a:move_arg
+      execute "normal! ".col_num."|"
+    endfunction
+
+    vnoremap <silent> <C-Up> :<C-u>call MoveVisualUp()<CR>
+    vnoremap <silent> <C-Down> :<C-u>call MoveVisualDown()<CR>
 
     nnoremap <C-S-d> :copy .<cr>
     vnoremap <C-S-d> :copy '><cr>
@@ -2365,7 +2416,7 @@ EOF
     "snoremap <C-V> <Space><Backspace><C-O>:call novim_mode#Paste()<CR>
     "cnoremap <C-V> <C-R>"
     "snoremap <C-C> <C-O>"+ygv
-    snoremap <C-X> <C-O>"+xi
+    "snoremap <C-X> <C-O>"+xi
 
     " Use <M-o><C-Z> for native terminal backgrounding.
     " The <Esc>s used in the `snoremap` commands seem to prevent the selection
