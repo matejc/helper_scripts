55e08215c12a9d341dc1ccb16e144969f2e97dc7
Matej Cotman <matej@matejc.com>
2023-02-08 22:21:28 +0200

    feat(jupyenv): use private netwroking

--- nixes/homemanager/modules/jupyenv.nix
+++ nixes/homemanager/modules/jupyenv.nix
@@ -6,19 +6,19 @@ with lib;
 let
   jupyenvOpts = { name, config, ... }: {
     options = {
-      enable = mkEnableOption (lib.mdDoc "Jupyter development server");
+      enable = mkEnableOption "Jupyter development server";
 
       ip = mkOption {
         type = types.str;
         default = "127.0.0.1";
-        description = lib.mdDoc ''
+        description = ''
           IP address Jupyter will be listening on.
         '';
       };
 
       port = mkOption {
         type = types.port;
-        description = lib.mdDoc ''
+        description = ''
           Port number Jupyter will be listening on.
         '';
       };
@@ -26,7 +26,7 @@ let
       uid = mkOption {
         type = types.int;
         default = 1000 + config.port;
-        description = lib.mdDoc ''
+        description = ''
           uid of user
         '';
       };
@@ -34,7 +34,7 @@ let
       gid = mkOption {
         type = types.int;
         default = 1000 + config.port;
-        description = lib.mdDoc ''
+        description = ''
           gid of user's group
         '';
       };
@@ -42,7 +42,7 @@ let
       uidMap = mkOption {
         type = types.int;
         default = 900000 + config.port;
-        description = lib.mdDoc ''
+        description = ''
           subuid of uid map
         '';
       };
@@ -50,7 +50,7 @@ let
       gidMap = mkOption {
         type = types.int;
         default = 900000 + config.port;
-        description = lib.mdDoc ''
+        description = ''
           subgid of gid map
         '';
       };
@@ -58,7 +58,7 @@ let
       password = mkOption {
         type = types.str;
         default = "u''";
-        description = lib.mdDoc ''
+        description = ''
           Password to use with notebook.
           Can be generated using:
             In [1]: from notebook.auth import passwd
@@ -75,7 +75,7 @@ let
       token = mkOption {
         type = types.str;
         default = "''";
-        description = lib.mdDoc ''
+        description = ''
           Token to use with notebook.
         '';
       };
@@ -83,7 +83,7 @@ let
       attrs = mkOption {
         type = types.attrs;
         default = {};
-        description = lib.mdDoc ''
+        description = ''
           Extra jupyenv attrset.
           https://jupyenv.io/documentation/how-to/
         '';
@@ -92,7 +92,7 @@ let
       extraConfig = mkOption {
         type = types.str;
         default = "";
-        description = lib.mdDoc ''
+        description = ''
           Extra config to pass as config file.
         '';
       };
@@ -165,10 +165,11 @@ in {
 
         runJupyterLab = pkgs.writeScript "jupyter-lab-${name}.sh" ''
           #!${pkgs.stdenv.shell}
+          set -e
           mkdir -p /home/j${name}/notebook
-          exec ${package}/bin/jupyter-lab \
+          ${package}/bin/jupyter-lab \
             --no-browser \
-            --ip=${cfg.ip} \
+            --ip=0.0.0.0 \
             --port=${toString cfg.port} --port-retries 0 \
             --notebook-dir=/home/j${name}/notebook \
             --config=${configFile}
@@ -193,6 +194,8 @@ in {
           coreutils-full
           bashInteractive
           git
+          gawk
+          iproute2
         ];
 
         pathEnv = pkgs.buildEnv {
@@ -213,13 +216,34 @@ in {
           nogroup:x:65534:nobody
         '';
 
-        startScript = pkgs.writeScript "start-jupyter-lab-${name}.sh" ''
-          #!${pkgs.stdenv.shell}
-          trap 'umount /var/lib/jupyenv-${name}/devpts' EXIT
-          mkdir -p /var/lib/jupyenv-${name}/devpts
-          mount -t devpts -o ptmxmode=0666 none /var/lib/jupyenv-${name}/devpts
+        slirp4netnsJson = builtins.toFile "json" (builtins.toJSON {
+          execute = "add_hostfwd";
+          arguments = {
+            proto = "tcp";
+            host_addr = cfg.ip;
+            host_port = cfg.port;
+            guest_port = cfg.port;
+          };
+        });
 
-          su - j${name} -c "${nsjail}/bin/nsjail \
+        startNs = pkgs.writeScript "start-ns-${name}.sh" ''
+          #!${pkgs.stdenv.shell}
+          trap 'kill $(jobs -rp)' EXIT
+
+          for i in {1..50}
+          do
+            nspid="$(${pkgs.procps}/bin/pgrep -u ${toString cfg.uidMap} -f ${runJupyterLab})"
+            if [ ! -z "$nspid" ] && [ -f "/proc/$nspid/cmdline" ]
+            then
+              ${pkgs.slirp4netns}/bin/slirp4netns --disable-dns \
+                --configure --mtu=65520 --disable-host-loopback \
+                --api-socket /var/lib/jupyenv-${name}/slirp4netns.sock $nspid tap0
+              break
+            fi
+            sleep 0.1
+          done &
+
+          { ${nsjail}/bin/nsjail \
             --uid_mapping ${toString cfg.uid}:${toString cfg.uidMap}:1 \
             --gid_mapping ${toString cfg.gid}:${toString cfg.gidMap}:1 \
             --bindmount_ro /nix/store:/nix/store \
@@ -232,10 +256,6 @@ in {
             --symlink ${passwdFile}:/etc/passwd \
             --symlink ${groupFile}:/etc/group \
             --symlink ${pathEnv}/bin:/bin \
-            --disable_clone_newnet \
-            --iface_no_lo \
-            --macvlan_iface br0 \
-            --macvlan_vs_ip 192.168.0.1 \
             --tmpfsmount /tmp \
             --tmpfsmount /home \
             --bindmount /var/lib/jupyenv-${name}/home:/home/j${name} \
@@ -246,7 +266,22 @@ in {
             --env LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries} \
             --rlimit_fsize 512 \
             --rlimit_nofile 512 \
-            -- ${runJupyterLab}"
+            -- ${runJupyterLab}; } 2> >(${pkgs.gnused}/bin/sed \
+              '/Use Control-C to stop this/ e cat ${slirp4netnsJson} | ${pkgs.libressl.nc}/bin/nc -U /var/lib/jupyenv-${name}/slirp4netns.sock')
+        '';
+
+        startScript = pkgs.writeScript "start-jupyter-lab-${name}.sh" ''
+          #!${pkgs.stdenv.shell}
+
+          on_exit() {
+            umount /var/lib/jupyenv-${name}/devpts
+          }
+          trap on_exit EXIT
+
+          mkdir -p /var/lib/jupyenv-${name}/devpts
+          mount -t devpts -o ptmxmode=0666,max=16 none /var/lib/jupyenv-${name}/devpts
+
+          su - j${name} -c "${startNs}"
         '';
       in nameValuePair "jupyenv-${name}" {
           description = "Jupyenv server - ${name}";
