44c0abefd888f94b8c0e625f836eca2e4072cfb4
Matej Cotman <matej.cotman@eficode.com>
2023-03-15 11:53:56 +0200

    feat: update

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -397,15 +397,6 @@ let
     exec "$@"
   '';
 
-  ansibleLintPy = pkgs.writeScript "ansible-lint.py" ''
-    import sys
-    from subprocess import run, PIPE
-
-    run(
-      [ '${pkgs.python3Packages.ansible-lint}/bin/ansible-lint' ] + sys.argv[1:],
-      shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    '';
-
   ginitVim = pkgs.writeText "ginit.vim" ''
     if exists('g:fvim_loaded')
       " Font tweaks
@@ -1999,42 +1990,40 @@ require('session_manager').setup({
   max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
 })
 
-require'treesitter-context'.setup{
-    enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-            'class',
-            'function',
-            'method',
-            -- 'for', -- These won't appear in the context
-            -- 'while',
-            -- 'if',
-            -- 'switch',
-            -- 'case',
-        },
-        -- Example for a specific filetype.
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        --   rust = {
-        --       'impl_item',
-        --   },
-    },
-    exact_patterns = {
-        -- Example for a specific filetype with Lua patterns
-        -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-        -- exactly match "impl_item" only)
-        -- rust = true,
-    },
-
-    -- [!] The options below are exposed but shouldn't require your attention,
-    --     you can safely ignore them.
-
-    zindex = 20, -- The Z-index of the context window
-}
+-- require'treesitter-context'.setup{
+--     enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
+--     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+--     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+--         -- For all filetypes
+--         -- Note that setting an entry here replaces all other patterns for this entry.
+--         -- By setting the 'default' entry below, you can control which nodes you want to
+--         -- appear in the context window.
+--         default = {
+--             'class',
+--             'function',
+--             'method',
+--             -- 'for', -- These won't appear in the context
+--             -- 'while',
+--             -- 'if',
+--             -- 'switch',
+--             -- 'case',
+--         },
+--         -- Example for a specific filetype.
+--         -- If a pattern is missing, *open a PR* so everyone can benefit.
+--         --   rust = {
+--         --       'impl_item',
+--         --   },
+--     },
+--     exact_patterns = {
+--         -- Example for a specific filetype with Lua patterns
+--         -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+--         -- exactly match "impl_item" only)
+--         -- rust = true,
+--     },
+--     -- [!] The options below are exposed but shouldn't require your attention,
+--     --     you can safely ignore them.
+--     zindex = 20, -- The Z-index of the context window
+-- }
 EOF
 
 lua <<EOF
@@ -2551,6 +2540,8 @@ EOF
     " highlight LineTooLongMarker guibg=Gray26
     " call matchadd('LineTooLongMarker', '\%81v', 100)
 
+    autocmd CursorHoldI * lua require('lspsaga.symbolwinbar'):get_winbar()
+
     autocmd UIEnter * source ${ginitVim}
 
     function! SetColumnToStart()
@@ -2672,7 +2663,7 @@ EOF
           vimPlugins.nvim-treesitter-textobjects
           vimPlugins.neovim-session-manager
           telescope-frecency-nvim
-          vimPlugins.nvim-treesitter-context
+          #vimPlugins.nvim-treesitter-context
           vimPlugins.novim-mode
           vimPlugins.lspsaga-nvim
           #vimPlugins.previm
