bb65b71244fcde5662c3f3a15b2b4028ad2289dd
Matej Cotman <matej@matejc.com>
2023-09-18 20:03:13 +0300

    wip: hyprland

--- nixes/homemanager/contexts/matej70-hyprland.nix
+++ nixes/homemanager/contexts/matej70-hyprland.nix
@@ -68,7 +68,7 @@ let
       vims = {
         q = "env QT_PLUGIN_PATH='${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}' ${pkgs.neovim-qt}/bin/nvim-qt --maximized --nvim ${homeDir}/bin/nvim";
         # n = ''${pkgs.neovide}/bin/neovide --neovim-bin "${homeDir}/bin/nvim" --frame none'';
-        g = "${pkgs.gnvim}/bin/gnvim --nvim ${homeDir}/bin/nvim --disable-ext-tabline --disable-ext-popupmenu --disable-ext-cmdline";
+        # g = "${pkgs.gnvim}/bin/gnvim --nvim ${homeDir}/bin/nvim --disable-ext-tabline --disable-ext-popupmenu --disable-ext-cmdline";
       };
       outputs = [{
         criteria = "HDMI-A-2";
