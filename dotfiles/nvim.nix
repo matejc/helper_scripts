754a252e96f80c4dedcadaadbb0849ce999976d0
Matej Cotman <matej@matejc.com>
2023-10-23 22:47:09 +0300

    feat: update

--- dotfiles/nvim.nix
+++ dotfiles/nvim.nix
@@ -1233,14 +1233,14 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ['<CR>'] = function(fallback)
-      if cmp.visible() and cmp.get_active_entry() then
+      if cmp.visible() then
         cmp.confirm({ select = false })
       else
         fallback()
       end
     end,
     ['<C-CR>'] = function(fallback)
-      if cmp.visible() and cmp.get_active_entry() then
+      if cmp.visible() then
         cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
       else
         fallback()
