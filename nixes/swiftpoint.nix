d1fbd46a0d7879add7cb9b809e90902e36caad5f
Matej Cotman <matej@matejc.com>
2024-05-06 21:43:45 +0300

    feat: add swiftpoint

---
+++ nixes/swiftpoint.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  pname = "swiftpoint";
+  version = "dev";
+
+  src = pkgs.fetchurl {
+    url = "https://swiftpointdrivers.blob.core.windows.net/pro/beta/linux/Swiftpoint%20X1%20Control%20Panel%203.0.6.2-0562612b.tar.xz";
+    sha256 = "sha256-Y1petsmCvpKl+iOEY/hJFm1eBRwRDBwidUEP+4RGmOI=";
+  };
+
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/swiftpoint
+    cp -r ./* $out/share/swiftpoint/
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      $out/share/swiftpoint/'Swiftpoint X1 Control Panel'
+    makeWrapper $out/share/swiftpoint/'Swiftpoint X1 Control Panel' $out/bin/swiftpoint \
+      --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath (with pkgs; [
+        libusb1 xorg.libX11 libgcc.lib libglvnd zlib glib fontconfig freetype xorg.libXext xorg.libXrender
+        xorg.libxcb libxkbcommon dbus libsForQt5.qt5.qtsvg openssl_1_1
+      ])}:$out/share/swiftpoint/lib" \
+      --set QT_PLUGIN_PATH "$out/share/swiftpoint/plugins" \
+      --set QT_QPA_PLATFORM_PLUGIN_PATH "$out/share/swiftpoint/plugins/platforms" \
+      --set QT_QPA_PLATFORM xcb
+  '';
+}
