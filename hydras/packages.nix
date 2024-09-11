255f3e4c318b5ac0e95f1d8ef30e62487bae5814
Matej Cotman <matej.cotman@eficode.com>
2024-09-11 17:13:56 +0300

    feat: update

--- hydras/packages.nix
+++ hydras/packages.nix
@@ -6,14 +6,15 @@
 let
   pkgs = import nixpkgs { inherit system; };
   rellib = import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems; };
-  nixpkgsFor = rellib.forAllSystems (system: import nixpkgs { inherit system; });
 
+  # nixpkgsFor = rellib.forAllSystems (system: import nixpkgs { inherit system; });
+  # jobs = rellib.forAllSystems (system: (builtins.listToAttrs (map (attr:
+  #   let
+  #     path = pkgs.lib.splitString "." attr;
+  #     p = pkgs.lib.getAttrFromPath path nixpkgsFor.${system};
+  #   in
+  #     {name = p.pname; value = builtins.listToAttrs (map (o: { name = o; value = p.${o}; }) p.outputs);}
+  # ) attrs)));
 
-  jobs = rellib.forAllSystems (system: (builtins.listToAttrs (map (attr:
-    let
-      path = pkgs.lib.splitString "." attr;
-      p = pkgs.lib.getAttrFromPath path nixpkgsFor.${system};
-    in
-      {name = p.pname; value = builtins.listToAttrs (map (o: { name = o; value = p.${o}; }) p.outputs);}
-  ) attrs)));
+  jobs = rellib.mapTestOn (builtins.listToAttrs (map (a: { name = a; value = rellib.linux; }) attrs));
 in jobs
