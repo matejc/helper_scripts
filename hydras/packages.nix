91cc13e402915863590d34c0c4eb854f236d18d4
Matej Cotman <matej@matejc.com>
2024-09-11 19:12:23 +0300

    feat: update

--- hydras/packages.nix
+++ hydras/packages.nix
@@ -1,20 +1,8 @@
 { nixpkgs ? <nixpkgs>
 , supportedSystems ? [ "x86_64-linux" ]
-, system ? builtins.currentSystem
-, attrs ? [ ]
+, attrs ? [ "curl" ]
 }:
+with import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems; };
 let
-  pkgs = import nixpkgs { inherit system; };
-  rellib = import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems; };
-
-  # nixpkgsFor = rellib.forAllSystems (system: import nixpkgs { inherit system; });
-  # jobs = rellib.forAllSystems (system: (builtins.listToAttrs (map (attr:
-  #   let
-  #     path = pkgs.lib.splitString "." attr;
-  #     p = pkgs.lib.getAttrFromPath path nixpkgsFor.${system};
-  #   in
-  #     {name = p.pname; value = builtins.listToAttrs (map (o: { name = o; value = p.${o}; }) p.outputs);}
-  # ) attrs)));
-
-  jobs = rellib.mapTestOn (builtins.listToAttrs (map (a: { name = a; value = supportedSystems; }) attrs));
+  jobs = mapTestOn (listToAttrs (map (a: { name = a; value = supportedSystems; }) attrs));
 in jobs
