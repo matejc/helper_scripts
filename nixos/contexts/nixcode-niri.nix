e11b09006f74a5b92f83321e4f110ee4b8a63139
Matej Cotman <matej.cotman@eficode.com>
2025-01-10 10:14:13 +0200

    feat: update

--- nixos/contexts/nixcode-niri.nix
+++ nixos/contexts/nixcode-niri.nix
@@ -80,6 +80,7 @@ let
       };
       vims = {
         q = "env QT_PLUGIN_PATH='${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}' ${pkgs.neovim-qt}/bin/nvim-qt --maximized --nvim ${self.variables.homeDir}/bin/nvim";
+        n = ''${pkgs.neovide}/bin/neovide --neovim-bin "${self.variables.homeDir}/bin/nvim" --frame none'';
       };
       outputs = [{
         criteria = "eDP-1";
