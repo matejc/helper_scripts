9df7430c4757ed9c47f0e43f032f5aa0e6feeb16
Matej Cotman <matej@matejc.com>
2025-04-12 03:01:07 +0300

    feat: update

--- nixes/openvpn-custom.nix
+++ nixes/openvpn-custom.nix
@@ -1,9 +1,14 @@
-{ pkgs ? import <nixpkgs> {
-  crossSystem = "armv6l-unknown-linux-musleabihf";
-} }:
-(pkgs.pkgsStatic.openvpn.overrideAttrs (old: {
-  configureFlags = ["--disable-plugin-auth-pam"];
-})).override {
-  useSystemd = false;
-  pam = null;
-}
+{ pkgs ? import <nixpkgs> { } }:
+let
+  pkgsArm32 = import <nixpkgs> {
+      crossSystem = "armv6l-unknown-linux-musleabihf";
+  };
+
+  openvpn = (pkgsArm32.pkgsStatic.openvpn.overrideAttrs (old: {
+      configureFlags = ["--disable-plugin-auth-pam"];
+  })).override {
+      useSystemd = false;
+      pam = null;
+  };
+in
+  openvpn
