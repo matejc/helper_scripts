e2a0bc5d2febbb64fa258a9041c4f8d4ecdee01a
Matej Cotman <matej@matejc.com>
2025-08-19 20:51:26 +0300

    feat: update

--- nixos/contexts/matej80-niri.nix
+++ nixos/contexts/matej80-niri.nix
@@ -156,8 +156,10 @@ let
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
+            user = "greeter";
           };
+          terminal.vt = lib.mkForce 2;
         };
       };
       programs.niri.enable = true;
