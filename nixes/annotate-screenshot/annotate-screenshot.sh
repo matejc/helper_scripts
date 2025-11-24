b70a880c4dbe28da9396aa901750b1842a39a4da
Matej Cotman <matej@matejc.com>
2025-11-24 19:28:35 +0200

    feat: update

--- nixes/annotate-screenshot/annotate-screenshot.sh
+++ nixes/annotate-screenshot/annotate-screenshot.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p "$HOME/Pictures/Screenshots"
 
-grim -g "$(slurp)" - | satty -f - --initial-tool=arrow --copy-command=wl-copy --actions-on-escape="save-to-file,save-to-clipboard,exit" --brush-smooth-history-size=5 --disable-notifications --output-filename "$HOME/Pictures/Screenshots/Annotated Screenshot From $(date '+%Y-%m-%d %H-%M-%S').png"
+grim -g "$(slurp)" - | satty -f - --initial-tool=arrow --copy-command=wl-copy --actions-on-escape="save-to-file,save-to-clipboard,exit" --brush-smooth-history-size=5 --disable-notifications --output-filename "$HOME/Pictures/Screenshots/Annotated screenshot from $(date '+%Y-%m-%d %H-%M-%S').png"
