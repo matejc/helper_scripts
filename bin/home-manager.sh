f4bd41671dde2b6d4e2c46ccc47221a00350bd42
Matej Cotman <matej@matejc.com>
2025-12-21 12:14:04 +0200

    feat: update

--- bin/home-manager.sh
+++ bin/home-manager.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if ! command -v nix >/dev/null 2>&1
 then
   sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
@@ -11,4 +13,4 @@ then
   nix --extra-experimental-features "nix-command flakes" run home-manager/master -- init --switch
 fi
 
-exec home-manager --extra-experimental-features "nix-command flakes" switch -b backup --flake "$1"
+exec home-manager --extra-experimental-features "nix-command flakes" switch -b backup --flake "${1:?'Error: Required flake path not supplied as first argument!'}"
