303a97ad7383cb2a4d9af99436a9d0dda92cd5f7
Matej Cotman <matej@matejc.com>
2026-03-27 18:54:31 +0200

    feat: update

--- contexts/matej70/home.nix
+++ contexts/matej70/home.nix
@@ -251,12 +251,13 @@ in
     programs.wireplumber = {
       enable = true;
       autoconnect.rules = [{
-        match = [ { application = "Firefox"; } { application = "Chromium input"; } ];
+        match = [ { application = "Firefox"; } { application = "Chromium input"; } { application = "ringrtc"; } ];
         sinks = [
           "bluez_output.20:74:CF:68:C4:4C"  # aftershockz
           "bluez_output.68:D6:ED:B9:AB:74"  # TWS
           "alsa_output.usb-Harman_International_Inc_JBL_Quantum_TWS_0000000000000000-00.analog-stereo"
           "alsa_output.pci-0000_00_1f.3.analog-stereo"
+          "alsa_output.pci-0000_09_00.0.analog-stereo"
         ];
         sources = [
           "alsa_input.usb-Jieli_Technology_USB_Composite_Device_4250323032373602-00.mono-fallback"
